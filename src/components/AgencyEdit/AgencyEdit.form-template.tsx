58350f11bfef0c77c6e009245039add6f646741b
Rail Sabirov <rail.sabirov@gmail.com>
2024-01-31 23:13:01 -0500

    Agency edit, disabling radio buttons

--- src/components/AgencyEdit/AgencyEdit.form-template.tsx
+++ src/components/AgencyEdit/AgencyEdit.form-template.tsx
@@ -1,160 +1,215 @@
-import { useTranslation } from 'react-i18next';
 import cn from 'classnames';
-import { Field, Form, ErrorMessage } from "formik";
+import { Field, Form, ErrorMessage } from 'formik';
 import Button from '../Button/Button';
-import styles from "./AgencyEdit.module.scss";
+import styles from './AgencyEdit.module.scss';
+import { useEffect } from 'react';
 
-const formTemplate = (values, errors) => {
-	const { t } = useTranslation();
-	const showFaxRadioButtonError = (faxName) => values[faxName] && !!errors.defaultFax && !values.defaultFax;
-	const showErrorMessage = (field: string, stylesString?: string) => errors[field]
-		? <div className={cn(styles['field-error-message'], stylesString)}>{errors[field]}</div>
-		: ''
+const formTemplate = (values, errors, t) => {
+	const showFaxRadioButtonError = (faxName) =>
+		values[faxName] && !!errors.defaultFax && !values.defaultFax;
+	const showErrorMessage = (field: string, stylesString?: string) =>
+		errors[field] ? (
+			<div className={cn(styles['field-error-message'], stylesString)}>
+				{errors[field]}
+			</div>
+		) : (
+			''
+		);
 
 	return (
 		<Form>
 			<div className={styles['main']}>
-
-
 				<label htmlFor="agencyNameField">{t('AgencyName')}</label>
-				<div className={cn(
-					styles['input-block'],
-					{
+				<div
+					className={cn(styles['input-block'], {
 						[styles['error-block-selector']]: !!errors.name
-					})} title={errors.name}>
-					<Field type="text" id="agencyNameField" name="name" required autoComplete="off" />
+					})}
+					title={errors.name}
+				>
+					<Field
+						type="text"
+						id="agencyNameField"
+						name="name"
+						required
+						autoComplete="off"
+					/>
 					{showErrorMessage('name')}
 				</div>
 
 				<label htmlFor="email">{t('Email')}</label>
-				<div className={cn(styles['input-block'], {
-					[styles['error-block-selector']]: !!errors.email
-				})}>
+				<div
+					className={cn(styles['input-block'], {
+						[styles['error-block-selector']]: !!errors.email
+					})}
+				>
 					<Field type="email" id="email" name="email" autoComplete="off" />
 					{showErrorMessage('email')}
 				</div>
 			</div>
 
-
 			<div className={styles['phone-fax']}>
 				<div className={styles['phone-numbers']}>
 					<span>{t('Phones')}</span>
-					<div className={cn(styles["group-headers"])}>
+					<div className={cn(styles['group-headers'])}>
 						<div className={styles['phone']}>{t('PhoneNumber')}</div>
 					</div>
 
 					<div className={styles['phone-fields']}>
 						<label htmlFor="phone1">{t('Phone1')}</label>
-						<div className={cn(styles["field"], {
-							[styles['error-block-selector']]: !!errors.phone1
-						})}>
+						<div
+							className={cn(styles['field'], {
+								[styles['error-block-selector']]: !!errors.phone1
+							})}
+						>
 							<Field type="tel" id="phone1" name="phone1" autoComplete="off" />
 							{showErrorMessage('phone1')}
 						</div>
 
 						<label htmlFor="phone1">{t('Phone2')}</label>
-						<div className={cn(styles["field"], {
-							[styles['error-block-selector']]: !!errors.phone2
-						})}>
+						<div
+							className={cn(styles['field'], {
+								[styles['error-block-selector']]: !!errors.phone2
+							})}
+						>
 							<Field type="tel" id="phone2" name="phone2" autoComplete="off" />
 							{showErrorMessage('phone2')}
 						</div>
 
 						<label htmlFor="phone1">{t('Phone3')}</label>
-						<div className={cn(styles["field"], {
-							[styles['error-block-selector']]: !!errors.phone3
-						})}>
+						<div
+							className={cn(styles['field'], {
+								[styles['error-block-selector']]: !!errors.phone3
+							})}
+						>
 							<Field type="tel" id="phone3" name="phone3" autoComplete="off" />
 							{showErrorMessage('phone3')}
 						</div>
 					</div>
 				</div>
 
-				<div className={styles["fax-numbers"]}>
+				<div className={styles['fax-numbers']}>
 					<span>{t('Faxes')}</span>
 
 					<div className={styles['rows']}>
-						<div className={cn(styles["default"], styles['header'])}>{t('Default')}</div>
-						<div className={cn(styles["label-column"], styles['header'])}>&nbsp;</div>
-						<div className={cn(styles["fax"], styles['header'])}>{t('FaxNumber')}</div>
+						<div className={cn(styles['default'], styles['header'])}>
+							{t('Default')}
+						</div>
+						<div className={cn(styles['label-column'], styles['header'])}>
+							&nbsp;
+						</div>
+						<div className={cn(styles['fax'], styles['header'])}>
+							{t('FaxNumber')}
+						</div>
 
 						<div className={cn(styles['colspan3'], styles['hr'])}></div>
 
-						<ErrorMessage name="defaultFax" component="div" className={cn(
-							styles['field-error-message'],
-							styles['colspan3'],
-							styles['default-fax-error'])} />
+						<ErrorMessage
+							name="defaultFax"
+							component="div"
+							className={cn(
+								styles['field-error-message'],
+								styles['colspan3'],
+								styles['default-fax-error']
+							)}
+						/>
 
-						{showErrorMessage('defaultFax', cn(
-							styles['field-error-message'],
-							styles['colspan3'],
-							styles['default-fax-error']))}
+						{showErrorMessage(
+							'defaultFax',
+							cn(
+								styles['field-error-message'],
+								styles['colspan3'],
+								styles['default-fax-error']
+							)
+						)}
 
-
-						<Field type="radio" name="defaultFax" value="fax1"
-							className={
-								cn(styles['radio-input'], {
-									[styles['radio-input-filled']]: showFaxRadioButtonError('fax1')
-								})} />
+						<Field
+							type="radio"
+							name="defaultFax"
+							value="fax1"
+							disabled={values.fax1?.length === 0}
+							className={cn(styles['radio-input'], {
+								[styles['radio-input-filled']]: showFaxRadioButtonError('fax1')
+							})}
+						/>
 						<label htmlFor="fax1">{t('Fax1')}</label>
-						<div className={cn(styles['right-side'], {
-							[styles['error-block-selector']]: !!errors.fax1
-						})}>
-							<Field type="tel" name="fax1" id="fax1" required autoComplete="off" />
+						<div
+							className={cn(styles['right-side'], {
+								[styles['error-block-selector']]: !!errors.fax1
+							})}
+						>
+							<Field
+								type="tel"
+								name="fax1"
+								id="fax1"
+								required
+								autoComplete="off"
+							/>
 							{showErrorMessage('fax1')}
 						</div>
 
-
-						<Field type="radio" name="defaultFax" value="fax2"
-							className={
-								cn(styles['radio-input'], {
-									[styles['radio-input-filled']]: showFaxRadioButtonError('fax2')
-								})} />
+						<Field
+							type="radio"
+							name="defaultFax"
+							value="fax2"
+							disabled={values.fax2?.length === 0}
+							className={cn(styles['radio-input'], {
+								[styles['radio-input-filled']]: showFaxRadioButtonError('fax2')
+							})}
+						/>
 						<label htmlFor="fax2">{t('Fax2')}</label>
-						<div className={cn(styles['right-side'], {
-							[styles['error-block-selector']]: !!errors.fax2
-						})}>
+						<div
+							className={cn(styles['right-side'], {
+								[styles['error-block-selector']]: !!errors.fax2
+							})}
+						>
 							<Field type="tel" name="fax2" id="fax2" autoComplete="off" />
 							{showErrorMessage('fax2')}
 						</div>
 
-
-						<Field type="radio" name="defaultFax" value="fax3"
-							className={
-								cn(styles['radio-input'], {
-									[styles['radio-input-filled']]: showFaxRadioButtonError('fax3')
-								})} />
+						<Field
+							type="radio"
+							name="defaultFax"
+							value="fax3"
+							disabled={values.fax3?.length === 0}
+							className={cn(styles['radio-input'], {
+								[styles['radio-input-filled']]: showFaxRadioButtonError('fax3')
+							})}
+						/>
 						<label htmlFor="fax3">{t('Fax3')}</label>
-						<div className={cn(styles['right-side'], {
-							[styles['error-block-selector']]: !!errors.fax3
-						})}>
+						<div
+							className={cn(styles['right-side'], {
+								[styles['error-block-selector']]: !!errors.fax3
+							})}
+						>
 							<Field type="tel" name="fax3" id="fax3" autoComplete="off" />
 							{showErrorMessage('fax3')}
 						</div>
 
-
-						<Field type="radio" name="defaultFax" value="fax4"
-							className={
-								cn(styles['radio-input'], {
-									[styles['radio-input-filled']]: showFaxRadioButtonError('fax4')
-								})} />
+						<Field
+							type="radio"
+							name="defaultFax"
+							value="fax4"
+							disabled={values.fax4?.length === 0}
+							className={cn(styles['radio-input'], {
+								[styles['radio-input-filled']]: showFaxRadioButtonError('fax4')
+							})}
+						/>
 						<label htmlFor="fax4">{t('Fax4')}</label>
-						<div className={cn(styles['right-side'], {
-							[styles['error-block-selector']]: !!errors.fax4
-						})}>
+						<div
+							className={cn(styles['right-side'], {
+								[styles['error-block-selector']]: !!errors.fax4
+							})}
+						>
 							<Field type="tel" name="fax4" id="fax4" autoComplete="off" />
 							{showErrorMessage('fax4')}
 						</div>
-
 					</div>
-
-
 				</div>
 			</div>
 
 			<div className={styles['comments']}>
 				<span>{t('Comment')}</span>
-				<div className={styles["row"]}>
+				<div className={styles['row']}>
 					<Field as="textarea" id="comment" name="comment" />
 				</div>
 			</div>
@@ -162,8 +217,7 @@ const formTemplate = (values, errors) => {
 			<div className={styles['submit-button']}>
 				<Button type="submit">{t('Save')}</Button>
 			</div>
-
-		</Form >
+		</Form>
 	);
 };
 
--- src/components/AgencyEdit/AgencyEdit.tsx
+++ src/components/AgencyEdit/AgencyEdit.tsx
@@ -1,94 +1,91 @@
 import formTemplate from './AgencyEdit.form-template';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
-import validate from './AgencyEdit.validation';
 import cn from 'classnames';
-import styles from "./AgencyEdit.module.scss";
+import styles from './AgencyEdit.module.scss';
 import { Formik } from 'formik';
 
-
 export default function AgencyEdit() {
-    const { t } = useTranslation();
+	const { t } = useTranslation();
 
-    const initialValues = {
-        name: "",
+	const initialValues = {
+		name: '',
 
-        fax1: "",
-        fax2: "",
-        fax3: "",
-        fax4: "",
-        defaultFax: "",
+		fax1: '',
+		fax2: '',
+		fax3: '',
+		fax4: '',
+		defaultFax: '',
 
-        phone1: "",
-        phone2: "",
-        phone3: "",
+		phone1: '',
+		phone2: '',
+		phone3: '',
 
-        email: "",
+		email: '',
 
-        comments: ""
-    };
+		comments: ''
+	};
 
-    const submitHandler = (values) => {
-        // Обработка отправки формы, например, отправка данных на сервер
-        console.log(values);
-    };
+	const submitHandler = (values) => {
+		// Обработка отправки формы, например, отправка данных на сервер
+		console.log(values);
+	};
 
+	const phoneFaxRule = (deviceType: 'Phone' | 'Fax', num: number) =>
+		Yup.string().test(
+			`${deviceType}${num}_test`,
+			t(`${deviceType} number must contain 10 digits`),
+			(value) =>
+				value == undefined || (value && /^\d{10}$/.exec(value)) ? true : false
+		);
 
-    const phoneFaxRule = (deviceType: 'Phone' | 'Fax', num: number) => Yup.string().test(
-        `${deviceType}${num}_test`,
-        t(`${deviceType} number must contain 10 digits`),
-        (value) => value == undefined || (value && value.length === 10) ? true : false
-    )
+	const validationScheme = Yup.object({
+		name: Yup.string()
+			.required(t('Enter the Agency Name'))
+			//.test("name_require_test", "Задайте Наименование агенства!", (value) => value !== undefined)
+			.test(
+				'name_min_3_letter_test',
+				t('The agency name must contain at least 3 letters'),
+				(value) => value?.length >= 3
+			),
 
-    const validationScheme = Yup.object({
+		email: Yup.string().email(t('Enter Email')),
 
-        name: Yup.string()
-            .required(t("Enter the Agency Name"))
-            //.test("name_require_test", "Задайте Наименование агенства!", (value) => value !== undefined)
-            .test("name_min_3_letter_test", t('The agency name must contain at least 3 letters'), (value) => value?.length >= 3),
+		phone1: phoneFaxRule('Phone', 1),
+		phone2: phoneFaxRule('Phone', 2),
+		phone3: phoneFaxRule('Phone', 3),
 
-        email: Yup.string().email(t('Enter Email')),
+		fax1: phoneFaxRule('Fax', 1).required(t('Enter your fax number')),
+		fax2: phoneFaxRule('Fax', 2),
+		fax3: phoneFaxRule('Fax', 3),
+		fax4: phoneFaxRule('Fax', 4),
 
-        phone1: phoneFaxRule('Phone', 1),
-        phone2: phoneFaxRule('Phone', 2),
-        phone3: phoneFaxRule('Phone', 3),
+		defaultFax: Yup.string().when(
+			['fax1', 'fax2', 'fax3', 'fax4'],
+			(faxFields, schema) => {
+				const [fax1, fax2, fax3, fax4] = faxFields;
 
-        fax1: phoneFaxRule('Fax', 1).required(t('Enter your fax number')),
-        fax2: phoneFaxRule('Fax', 2),
-        fax3: phoneFaxRule('Fax', 3),
-        fax4: phoneFaxRule('Fax', 4),
+				return schema.test({
+					test: (value) => {
+						// Поле defaultFax обязательно, если заполнено поле fax1 или fax2 или fax3 или fax4
+						return fax1 || fax2 || fax3 || fax4 ? !!value : true;
+					},
+					message: t('Error: No default fax selected!')
+				});
+			}
+		)
+	});
 
-        defaultFax: Yup.string().when(
-            ['fax1', 'fax2', 'fax3', 'fax4'],
-            (faxFields, schema) => {
-                const [fax1, fax2, fax3, fax4] = faxFields;
-
-                return schema.test({
-                    test: value => {
-                        // Поле defaultFax обязательно, если заполнено поле fax1 или fax2 или fax3 или fax4
-                        return (fax1 || fax2 || fax3 || fax4) ? !!value : true;
-                    },
-                    message: t('Error: No default fax selected!'),
-                });
-            }),
-    });
-
-    return (
-        <div className={cn(styles['agency'], styles['edit-mode'])}>
-
-            <Formik
-                initialValues={initialValues}
-                //validationSchema={validate()}
-                validationSchema={validationScheme}
-                validateOnChange={true}
-                onSubmit={submitHandler}
-            >
-                {({ values, errors }) => (
-                    formTemplate(values, errors)
-                )}
-
-            </Formik>
-
-        </div>
-    )
+	return (
+		<div className={cn(styles['agency'], styles['edit-mode'])}>
+			<Formik
+				initialValues={initialValues}
+				validationSchema={validationScheme}
+				validateOnChange={true}
+				onSubmit={submitHandler}
+			>
+				{({ values, errors }) => formTemplate(values, errors, t)}
+			</Formik>
+		</div>
+	);
 }
